cbcb95f5aed8188e7f4b4c75cf94aa7353fca9cf
Enzo Rossetto <enzo.rossetto@ifood.com.br>
2024-09-10 21:21:58 -0300

    chore: implement xid for IDs and add more tests

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -2,11 +2,12 @@ package campaign
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewCampaign(t *testing.T) {
+func Test_NewCampaign_CreateCampaign(t *testing.T) {
 	assert := assert.New(t)
 	name := "New Campaign"
 	content := "Hello, World!"
@@ -14,8 +15,30 @@ func TestNewCampaign(t *testing.T) {
 
 	campaign := NewCampaign(name, content, emails)
 
-	assert.Equal(campaign.ID, "1")
 	assert.Equal(campaign.Name, name)
 	assert.Equal(campaign.Content, content)
 	assert.Equal(len(campaign.Contacts), len(emails))
+}
+
+func Test_NewCampaign_IDIsNotNil(t *testing.T) {
+	assert := assert.New(t)
+	name := "New Campaign"
+	content := "Hello, World!"
+	emails := []string{"mail1@m.com", "mail2@m.com"}
+
+	campaign := NewCampaign(name, content, emails)
+
+	assert.NotNil(campaign.ID)
+}
+
+func Test_NewCampaign_CreatedAtIsNotNil(t *testing.T) {
+	assert := assert.New(t)
+	name := "New Campaign"
+	content := "Hello, World!"
+	emails := []string{"mail1@m.com", "mail2@m.com"}
+	now := time.Now().Add(-time.Minute)
+
+	campaign := NewCampaign(name, content, emails)
+
+	assert.Greater(campaign.CreatedAt, now)
 }

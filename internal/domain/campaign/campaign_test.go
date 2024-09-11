aaf01793c7ff66368dbb3ced755432662fa6539d
Enzo Rossetto <enzo.rossetto@ifood.com.br>
2024-09-10 21:07:09 -0300

    test: add testify to project

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -1,21 +1,21 @@
 package campaign
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestNewCampaign(t *testing.T) {
+	assert := assert.New(t)
 	name := "New Campaign"
 	content := "Hello, World!"
 	emails := []string{"mail1@m.com", "mail2@m.com"}
 
 	campaign := NewCampaign(name, content, emails)
 
-	if campaign.ID != "1" {
-		t.Errorf("Campaign ID is not 1")
-	} else if campaign.Name != name {
-		t.Errorf("Campaign Name is not %s", name)
-	} else if campaign.Content != content {
-		t.Errorf("Campaign Content is not %s", content)
-	} else if len(campaign.Contacts) != len(emails) {
-		t.Errorf("Campaign Contacts length is not %d", len(emails))
-	}
+	assert.Equal(campaign.ID, "1")
+	assert.Equal(campaign.Name, name)
+	assert.Equal(campaign.Content, content)
+	assert.Equal(len(campaign.Contacts), len(emails))
 }
